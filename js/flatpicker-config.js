7ebbbc741c9d6707c9d6f5e70ea6e2dabbab0b3d
Frank Molenaar <info@frankmolenaar.net>
2022-03-15 18:47:59 +0100

    added basic flatpicker config

--- js/flatpicker-config.js
+++ js/flatpicker-config.js
@@ -1,8 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const currentDate = new Date();
+
     flatpickr("#myDateTimePicker", { 
-        dateFormat:		"n/j/Y H:i", 
-        enableTime:		true, 
+        // Documentation: https://flatpickr.js.org/
+        // General settings
         allowInput:		true,
-        theme: 			"material_green"
+        theme: 			"material_green",      
+        
+        // Date settings
+        minDate:        "today", 
+        disable:        [
+            function(date) {
+                // return true to disable
+                return (date.getDay() === 0 || date.getDay() === 6);
+    
+            }
+        ],
+        
+        // Time Settings
+        enableTime:     true,
+        time_24hr:      true,
+        minTime:        "9:00",
+        maxTime:        "17:00"
+                
     });
 });
